2d65c878ea94e18c1b046bd2e1255eae10e668a2
Acul221 <hasrullahazis@gmail.com>
2025-04-27 00:47:39 +0700

    Update current.js

--- netlify/functions/current.js
+++ netlify/functions/current.js
@@ -1,14 +1,14 @@
 // netlify/functions/current.js
 export default async function handler(req, res) {
   const { lat, lon } = req.query;
-  const openWeatherKey = process.env.OPENWEATHERMAP_API_KEY;
+  const apiKey = process.env.OPENWEATHERMAP_API_KEY;
 
-  if (!lat || !lon || !openWeatherKey) {
+  if (!lat || !lon || !apiKey) {
     return res.status(400).json({ error: "Missing latitude, longitude, or API key" });
   }
 
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherKey}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
     const data = await response.json();
     res.status(200).json({
       temp: data.main.temp,
