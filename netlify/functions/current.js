54c4eb593c04a87eed07006804784d838d67afb8
Acul221 <hasrullahazis@gmail.com>
2025-04-26 23:36:49 +0700

    Update current.js

--- netlify/functions/current.js
+++ netlify/functions/current.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPENWEATHERMAP_KEY}`);
     const data = await response.json();
 
     res.status(200).json({
